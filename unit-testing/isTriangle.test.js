fc07c40e7bdbd20b8b5ad441413f1046f8cbc09f
IrenaGau <naudotojas@yahoo.com>
2022-07-20 10:06:46 +0300

    Pirmos paskaitos papildymai

--- unit-testing/isTriangle.test.js
+++ unit-testing/isTriangle.test.js
@@ -4,6 +4,22 @@ test('3, 4, 5 is triangle', () => {
     expect(isTriangle(3, 4, 5)).toBe(true);
 });
 
-test('3, 4, 5 is triangle', () => {
-    expect(isTriangle(3, 4, 5)).toBe(false);
+test('0, 4, 5 is not triangle', () => {
+    expect(isTriangle(0, 4, 5)).toBe(false);
+});
+
+test('-4, 4, 5 is not triangle', () => {
+    expect(isTriangle(-4, 4, 5)).toBe(false);
+});
+
+test('A, 4, 5 is not triangle', () => {
+    expect(isTriangle('A', 4, 5)).toBe(false);
+});
+
+test('10000000, 4, 5 is not triangle', () => {
+    expect(isTriangle(10000000, 4, 5)).toBe(false);
+});
+
+test('6,5, 4, 5 is not triangle', () => {
+    expect(isTriangle(6,5, 4, 5)).toBe(true);
 });
