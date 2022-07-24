86347542b795ff8238ad13b68e96181ba2483724
IrenaGau <naudotojas@yahoo.com>
2022-07-25 00:00:13 +0300

    Tests

--- homework/tests/example.spec.js
+++ homework/tests/example.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+/*const { test, expect } = require('@playwright/test');
 
 const data = [
   'Prototype',
@@ -28,4 +28,4 @@ data.forEach(version => {
       await expect(page.locator('#numberAnswerField')).toHaveValue('5');
     });
   });
-});
+});*/
