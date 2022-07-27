141e9eb96110e32693bc5cc3fe0eb5efe63f9cdb
IrenaGau <naudotojas@yahoo.com>
2022-07-27 19:20:18 +0300

    Homework updated

--- homework/tests/IrenosHomework.spec.js
+++ homework/tests/IrenosHomework.spec.js
@@ -1,5 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://testsheepnz.github.io/BasicCalculator');
+});
+
 const data = [
   'Prototype',
   '1',
@@ -17,7 +21,7 @@ const data = [
 data.forEach(version => {
   test.describe(version + ': Add', () => {
     test('Adding 5 and 7 result is 12', async ({ page }) => {
-      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      //await page.goto('https://testsheepnz.github.io/BasicCalculator');
       await page.selectOption('#selectBuild', { label: version});
       await page.locator('#number1Field').type('5');
       await page.locator('#number2Field').type('7');
@@ -26,13 +30,36 @@ data.forEach(version => {
   
       await expect(page.locator('#numberAnswerField')).toHaveValue('12');
     });
+
+    test('When adding not a number error message should be displayed', async ({ page }) => {
+      //await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('a');
+      await page.locator('#number2Field').type('7');
+      await page.selectOption('#selectOperationDropdown', {label: 'Add'});
+      await page.locator('#calculateButton').click();
+      
+      await expect(page.locator('#errorMsgField')).toContainText('Number 1 is not a number');
+    });
+
+    test('When clicking calculate button multiple times result should remain the same', async ({ page }) => {
+      //await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('2');
+      await page.locator('#number2Field').type('2');
+      await page.selectOption('#selectOperationDropdown', {label: 'Add'});
+      await page.locator('#calculateButton').click();
+      await page.locator('#calculateButton').click();
+      await page.locator('#calculateButton').click();
+      await expect(page.locator('#numberAnswerField')).toHaveValue('4');
+    });
   });
 });
 
 data.forEach(version => {
   test.describe(version + ': Subtract', () => {
     test('Subtracting 7 and 3 result is 4', async ({ page }) => {
-      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      //await page.goto('https://testsheepnz.github.io/BasicCalculator');
       await page.selectOption('#selectBuild', { label: version});
       await page.locator('#number1Field').type('7');
       await page.locator('#number2Field').type('3');
@@ -41,13 +68,25 @@ data.forEach(version => {
     
       await expect(page.locator('#numberAnswerField')).toHaveValue('4');
     });
+
+    test('Should clear answer field after clicking "clear" button', async ({ page }) => {
+      //await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('1');
+      await page.locator('#number2Field').type('6');
+      await page.selectOption('#selectOperationDropdown', {label: 'Subtract'});
+      await page.locator('#calculateButton').click();
+      await page.locator('#clearButton').click();
+      
+      await expect(page.locator('#numberAnswerField')).toHaveValue('');
+    });
   });
 });
 
 data.forEach(version => {
   test.describe(version + ': Multiply', () => {
     test('Multiplying 3 and 2 result is 6', async ({ page }) => {
-      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      //await page.goto('https://testsheepnz.github.io/BasicCalculator');
       await page.selectOption('#selectBuild', { label: version});
       await page.locator('#number1Field').type('3');
       await page.locator('#number2Field').type('2');
@@ -62,7 +101,7 @@ data.forEach(version => {
 data.forEach(version => {
   test.describe(version + ': Divide', () => {
     test('Dividing 9 and 2 result is 4.5', async ({ page }) => {
-      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      //await page.goto('https://testsheepnz.github.io/BasicCalculator');
       await page.selectOption('#selectBuild', { label: version});
       await page.locator('#number1Field').type('9');
       await page.locator('#number2Field').type('2');
@@ -71,44 +110,9 @@ data.forEach(version => {
     
       await expect(page.locator('#numberAnswerField')).toHaveValue('4.5');
     });
-  });
-});
 
-data.forEach(version => {
-  test.describe(version + ': Concatenate', () => {
-    test('Concatenating 5 and 3 results in 53', async ({ page }) => {
-      await page.goto('https://testsheepnz.github.io/BasicCalculator');
-      await page.selectOption('#selectBuild', { label: version});
-      await page.locator('#number1Field').type('5');
-      await page.locator('#number2Field').type('3');
-      await page.selectOption('#selectOperationDropdown', {label: 'Concatenate'});
-      await page.locator('#calculateButton').click();
-    
-      await expect(page.locator('#numberAnswerField')).toHaveValue('53');
-    });
-  });
-});
-
-data.forEach(version => {
-  test.describe(version + ': Subtract', () => {
-    test('Test Clear button', async ({ page }) => {
-      await page.goto('https://testsheepnz.github.io/BasicCalculator');
-      await page.selectOption('#selectBuild', { label: version});
-      await page.locator('#number1Field').type('1');
-      await page.locator('#number2Field').type('6');
-      await page.selectOption('#selectOperationDropdown', {label: 'Subtract'});
-      await page.locator('#calculateButton').click();
-      await page.locator('#clearButton').click();
-      
-      await expect(page.locator('#numberAnswerField')).toHaveValue('');
-    });
-  });
-});
-
-data.forEach(version => {
-  test.describe(version + ': Divide', () => {
-    test('Test Integer only check box', async ({ page }) => {
-      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+    test('After clicking "Integers only" check box, answer should be diplayed as integer', async ({ page }) => {
+      //await page.goto('https://testsheepnz.github.io/BasicCalculator');
       await page.selectOption('#selectBuild', { label: version});
       await page.locator('#number1Field').type('7');
       await page.locator('#number2Field').type('3');
@@ -118,44 +122,9 @@ data.forEach(version => {
       
       await expect(page.locator('#numberAnswerField')).toHaveValue('2');
     });
-  });
-});
 
-data.forEach(version => {
-  test.describe(version + ':Add', () => {
-    test('When adding not number error message should be displayed', async ({ page }) => {
-      await page.goto('https://testsheepnz.github.io/BasicCalculator');
-      await page.selectOption('#selectBuild', { label: version});
-      await page.locator('#number1Field').type('a');
-      await page.locator('#number2Field').type('7');
-      await page.selectOption('#selectOperationDropdown', {label: 'Add'});
-      await page.locator('#calculateButton').click();
-      
-      await expect(page.locator('#errorMsgField')).toContainText('Number 1 is not a number');
-    });
-  });
-});
-
-data.forEach(version => {
-  test.describe(version + ': Add', () => {
-    test('When clicking calculate button multiple times result should remain the same', async ({ page }) => {
-      await page.goto('https://testsheepnz.github.io/BasicCalculator');
-      await page.selectOption('#selectBuild', { label: version});
-      await page.locator('#number1Field').type('2');
-      await page.locator('#number2Field').type('2');
-      await page.selectOption('#selectOperationDropdown', {label: 'Add'});
-      await page.locator('#calculateButton').click();
-      await page.locator('#calculateButton').click();
-      await page.locator('#calculateButton').click();
-      await expect(page.locator('#numberAnswerField')).toHaveValue('4');
-    });
-  });
-});
-
-data.forEach(version => {
-  test.describe(version + ': Divide', () => {
     test('When dividing from 0 error message should be displayed', async ({ page }) => {
-      await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      //await page.goto('https://testsheepnz.github.io/BasicCalculator');
       await page.selectOption('#selectBuild', { label: version});
       await page.locator('#number1Field').type('8');
       await page.locator('#number2Field').type('0');
@@ -163,6 +132,21 @@ data.forEach(version => {
       await page.locator('#calculateButton').click();
       
       await expect(page.locator('#errorMsgField')).toContainText('Divide by zero error!');
+    });
+  });
+});
+
+data.forEach(version => {
+  test.describe(version + ': Concatenate', () => {
+    test('Concatenating 5 and 3 results in 53', async ({ page }) => {
+      //await page.goto('https://testsheepnz.github.io/BasicCalculator');
+      await page.selectOption('#selectBuild', { label: version});
+      await page.locator('#number1Field').type('5');
+      await page.locator('#number2Field').type('3');
+      await page.selectOption('#selectOperationDropdown', {label: 'Concatenate'});
+      await page.locator('#calculateButton').click();
+    
+      await expect(page.locator('#numberAnswerField')).toHaveValue('53');
     });
   });
 });
